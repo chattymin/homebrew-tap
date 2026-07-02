4866565f8c9b1313eee86244f178987d58f914cb
Patrick.Park <pdm001125@naver.com>
2026-07-02 17:44:06 +0900

    cask: poke-token-bar 2.2.2

--- Casks/poke-token-bar.rb
+++ Casks/poke-token-bar.rb
@@ -1,5 +1,5 @@
 cask "poke-token-bar" do
-  version "2.2.1"
+  version "2.2.2"
   sha256 :no_check
 
   url "https://github.com/chattymin/PokeTokenBar/releases/download/v#{version}/PokeTokenBar.zip"
